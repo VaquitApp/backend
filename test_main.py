5462ce56ad458dc42e8cf67e3e77b80518d19e9c
Santiago <santiago.bellido@hotmail.com>
2024-05-07 23:19:30 -0300

    add test db + set up before each test

--- test_main.py
+++ test_main.py
@@ -1,7 +1,42 @@
 from fastapi.testclient import TestClient
-from .main import app
-import os
-import time
+import pytest
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+
+from .database import Base
+from .main import app, get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def set_up_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
@@ -9,7 +44,7 @@ client = TestClient(app)
 # REGISTRATION
 ################################################
 
-def test_register_a_user():
+def test_register_a_user(set_up_db):
     response = client.post(
         url = "/user/register",
         json = {"email":"example@example.com", "password":"my_ultra_secret_password"},
@@ -17,7 +52,7 @@ def test_register_a_user():
     assert response.status_code == 200
     assert 'id' in response.json()
 
-def test_register_a_user_with_an_email_already_used():
+def test_register_a_user_with_an_email_already_used(set_up_db):
     first_response = client.post(
         url = "/user/register",
         json = {"email":"example@example.com", "password":"my_ultra_secret_password"},
@@ -37,7 +72,7 @@ def test_register_a_user_with_an_email_already_used():
 # LOGIN
 ################################################
 
-def test_successful_login():
+def test_successful_login(set_up_db):
     first_response = client.post(
         url = "/user/register",
         json = {"email":"example@example.com", "password":"my_ultra_secret_password"},
@@ -55,7 +90,7 @@ def test_successful_login():
     assert 'token' in second_response.json()
 
 
-def test_login_with_wrong_password():
+def test_login_with_wrong_password(set_up_db):
     first_response = client.post(
         url = "/user/register",
         json = {"email":"example@example.com", "password":"my_ultra_secret_password"},
@@ -72,7 +107,7 @@ def test_login_with_wrong_password():
     assert second_response.status_code == 404
     assert 'token' not in second_response.json()
 
-def test_login_with_wrong_email():
+def test_login_with_wrong_email(set_up_db):
     first_response = client.post(
         url = "/user/register",
         json = {"email":"example@example.com", "password":"my_ultra_secret_password"},
