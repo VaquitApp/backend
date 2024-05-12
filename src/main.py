b6c89e6b6f460b37bfbfbaecc12b3916b813e36e
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2024-05-12 17:11:42 -0300

    refactor: change messages to spanish, change status to 201

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from fastapi import Depends, FastAPI, HTTPException
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -19,28 +20,28 @@ def get_db():
 app = FastAPI(dependencies=[Depends(get_db)])
 
 
-@app.post("/user/register")
+@app.post("/user/register", status_code=HTTPStatus.CREATED)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
 
     if db_user is not None:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=400, detail="Email ya existente")
 
     db_user = crud.create_user(db, user)
 
     return {"id": db_user.id}
 
 
-@app.post("/user/login")
+@app.post("/user/login", status_code=HTTPStatus.CREATED)
 def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
 
     if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Usuario no existe")
 
     hashed_password = hashlib.sha256(user.password.encode(encoding="utf-8")).hexdigest()
 
     if db_user.password != hashed_password:
-        raise HTTPException(status_code=404, detail="Verify your credentials")
+        raise HTTPException(status_code=404, detail="Contraseña incorrecta")
 
     return {"token": db_user.id}
